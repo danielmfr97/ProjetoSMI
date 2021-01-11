46193f760d2e7a2b60706dbf6c4cf84caa3df9cd
Daniel Ramos <danielmfr6@gmail.com>
2021-01-11 18:57:57 -0300

    first commit

---
+++ app/src/main/java/br/com/danielramos/projetosmi/ui/main/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.danielramos.projetosmi.ui.main.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.danielramos.projetosmi.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/br/com/danielramos/projetosmi/utils/Status.kt
@@ -0,0 +1,7 @@
+package br.com.danielramos.projetosmi.utils
+
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
