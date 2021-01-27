cfade2bded2ce96ba859d3b6bebea72174a53712
Daniel Ramos <danielmfr6@gmail.com>
2021-01-27 08:34:53 -0300

    Inicializar o Koin

--- app/src/main/java/br/com/danielramos/projetosmi/MyApplication.kt
+++ app/src/main/java/br/com/danielramos/projetosmi/MyApplication.kt
@@ -1,14 +1,20 @@
 package br.com.danielramos.projetosmi
 
 import android.app.Application
+import br.com.danielramos.projetosmi.di.mainModule
 import io.realm.Realm
 import io.realm.RealmConfiguration
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         configurarRealm()
+        configurarKoin()
     }
 
     private fun configurarRealm() {
@@ -16,6 +22,16 @@ class MyApplication : Application() {
         val configuration = RealmConfiguration.Builder()
             .name("projeto.realm").build()
         Realm.setDefaultConfiguration(configuration)
+    }
+
+    private fun configurarKoin() {
+        startKoin{
+            androidLogger(Level.DEBUG)
+            androidContext(this@MyApplication)
+            modules(
+                listOf(mainModule)
+            )
+        }
     }
 
 }
